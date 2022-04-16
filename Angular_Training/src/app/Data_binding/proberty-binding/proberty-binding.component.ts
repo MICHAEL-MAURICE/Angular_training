f750991222bbd661ac7085b1c41939f23efe5fbb
MICHAEL-MAURICE <52583744+MICHAEL-MAURICE@users.noreply.github.com>
2022-04-16 22:45:58 +0200

    classBinding&& StyleBinding

--- Angular_Training/src/app/Data_binding/proberty-binding/proberty-binding.component.ts
+++ Angular_Training/src/app/Data_binding/proberty-binding/proberty-binding.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-proberty-binding',
   templateUrl: './proberty-binding.component.html',
-  styleUrls: ['./proberty-binding.component.css']
+  styleUrls: ['./proberty-binding.component.css'],
 })
 export class ProbertyBindingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  isDisabled: boolean = true;
+  ngOnInit(): void {}
 }
--- Angular_Training/src/app/app.module.ts
+++ Angular_Training/src/app/app.module.ts
@@ -4,21 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
-import { ProbertyBindingComponent } from './proberty-binding/proberty-binding.component';
+import { InterpolationBindingComponent } from './Data_binding/interpolation-binding/interpolation-binding.component';
+import { ProbertyBindingComponent } from './Data_binding/proberty-binding/proberty-binding.component';
+import { ClassBindingComponent } from './Data_binding/class-binding/class-binding.component';
+import { StyleBindingComponent } from './Data_binding/style-binding/style-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     InterpolationBindingComponent,
-    ProbertyBindingComponent
+    ProbertyBindingComponent,
+    ClassBindingComponent,
+    StyleBindingComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
